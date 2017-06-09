4621c318a491922dbf1cc69d648c9ccb417ec5ae
jhendley25 <jake.hendley25@gmail.com>
2017-06-09 11:57:24 -0400

    drop in prescribed routes

--- search-api/config/routes.rb
+++ search-api/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :apps
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :apps
+  
+  scope("/api/1") do
+    get '/apps' => 'apps#index'
+    put '/apps/:id' => 'apps#update'
+    post '/apps' => 'apps#create'
+    delete '/apps/:id' => 'apps#destroy'
+  end
 end
